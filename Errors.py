548c252c13465bf5813df5fc12c4bf4453ed19de
Aryan Deshmukh <aryandeshmukh0987@gmail.com>
2026-07-27 05:38:39 +0530

    Add files via upload

---
+++ Errors.py
@@ -0,0 +1,7 @@
+while True:
+    try:
+        a = int(input("Enter no.1: "))
+        b = int(input("Enter no.2: "))
+        print(f"The result is {a+b}")
+    except:
+        print("An error Occured!")
